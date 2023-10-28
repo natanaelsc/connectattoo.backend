97ba172a365f6ba8235e56cdf612aa184fa07e3b
natanaelsc <natanael96@live.com>
2023-10-28 20:50:07 -0300

    chore: add exceptions handlers and user.repository

--- src/modules/user/create-user/create-user.service.ts
+++ src/modules/user/create-user/create-user.service.ts
@@ -1,85 +1,61 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable
+} from '@nestjs/common';
 import bcrypt from 'bcrypt';
-import { randomUUID } from 'crypto';
-import { AppService } from 'src/app.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '../user.interface';
+import { UserRepository } from '../user.repository';
+import { CreateUser } from './create-user';
 import { CreateUserDto } from './create-user.dto';
 
 @Injectable()
 export class CreateUserService {
-  private confirmationKey: string;
-  private email: string;
-  constructor(
-    private readonly appService: AppService,
-    private readonly prisma: PrismaService,
-  ) {}
+  constructor(private readonly userRepository: UserRepository) {}
 
-  public async create(createUserDto: CreateUserDto) {
+  public async create(createUserDto: CreateUserDto): Promise<User> {
+    const email = createUserDto.email;
+    const emailAlreadyInUse = await this.userRepository.findByEmail(email);
+
+    if (emailAlreadyInUse !== null) {
+      throw new BadRequestException(
+        `Usuário com email '${emailAlreadyInUse.email}' já cadastrado`,
+      );
+    }
+
     const name = createUserDto.name.split(' ');
     const firstName = name[0];
     const lastName = name[1];
+
+    this.comparePasswords(
+      createUserDto.password,
+      createUserDto.passwordConfirmation,
+    );
+
     const password = await bcrypt.hash(createUserDto.password, 10);
+
     delete createUserDto.password;
-    const birthday = new Date(createUserDto.birthDate)
-      .toISOString()
-      .slice(0, 10);
-    const data = {
-      email: createUserDto.email,
-      password,
-      name: firstName,
+    delete createUserDto.passwordConfirmation;
+
+    const createUser = CreateUser.create(
+      firstName,
       lastName,
-      birthday,
-      confirmation_key: randomUUID().replace(/-/g, ''),
-      confirmation_sit: 0,
-    };
-    await this.isEmailFound(data);
+      email,
+      password,
+      createUserDto.birthDate,
+    );
 
-    const createdUser = await this.prisma.user
-      .create({ data })
-      .catch(async (error) => {
-        throw new HttpException(
-          {
-            status: HttpStatus.BAD_REQUEST,
-            error: 'Erro no cadastro do usuário',
-          },
-          HttpStatus.BAD_REQUEST,
-          {
-            cause: error,
-          },
-        );
-      });
-
-    await this.appService.sendEmail(data.email, data.confirmation_key);
-
-    this.confirmationKey = data.confirmation_key;
-    this.email = data.email;
-    return {
-      ...createdUser,
-      password: undefined,
-    };
+    const user = await this.userRepository.create(createUser);
+    return user;
   }
 
-  async isEmailFound(data: { email: string }): Promise<void> {
-    const dataBaseEmail = await this.prisma.user.findMany({
-      select: { email: true },
-    });
-
-    const listEmailDataBase: any[] = [];
-    dataBaseEmail.forEach((item) => {
-      const foundEmailDatabase = item.email;
-      listEmailDataBase.push(foundEmailDatabase);
-    });
-
-    const existEmailInDataBase = listEmailDataBase.includes(data.email);
-
-    if (existEmailInDataBase) {
-      throw new HttpException(
-        {
-          status: HttpStatus.BAD_REQUEST,
-          error: 'E-mail já cadastrado',
-        },
-        HttpStatus.BAD_REQUEST,
-      );
+  private comparePasswords(
+    password: string,
+    passwordConfirmation: string,
+  ): boolean {
+    if (password !== passwordConfirmation) {
+      throw new BadRequestException('Senhas não conferem');
     }
+    return true;
   }
+
 }
