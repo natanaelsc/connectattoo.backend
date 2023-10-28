eed33335e13ce13fdf31121233b35c95ce9a87f4
natanaelsc <natanael96@live.com>
2023-10-28 17:37:42 -0300

    feat(mail): create method implementation to send emails

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,4 +1,17 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { MailDto } from './mail.dto';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(private readonly mailerService: MailerService) {}
+
+  public async sendEmail(mailDto: MailDto): Promise<void> {
+    await this.mailerService.sendMail({
+      from: `"${process.env.MAIL_FROM_NAME}" <${process.env.MAIL_FROM_ADDRESS}>`,
+      to: mailDto.email,
+      subject: mailDto.subject,
+      html: mailDto.html,
+    });
+  }
+}
