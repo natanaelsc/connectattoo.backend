487e000b49cfc62eacd4ee791c653a2979f669d3
natanaelsc <natanael96@live.com>
2023-10-28 20:53:50 -0300

    chore(api): add users/register endpoint to user registrations

--- src/modules/user/create-user/create-user.controller.ts
+++ src/modules/user/create-user/create-user.controller.ts
@@ -3,13 +3,13 @@ import { IsPublic } from 'src/modules/auth/decorators/is-public.decorator';
 import { CreateUserDto } from './create-user.dto';
 import { CreateUserService } from './create-user.service';
 
-@Controller('create-user')
+@Controller('users')
 export class CreateUserController {
   constructor(private readonly createUserService: CreateUserService) {}
 
   @IsPublic()
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.createUserService.create(createUserDto);
+  @Post('/register')
+  async create(@Body() createUserDto: CreateUserDto): Promise<void> {
+    await this.createUserService.create(createUserDto);
   }
 }
