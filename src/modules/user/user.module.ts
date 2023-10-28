ad898b469f5538cefd731c3a14240117c9cf4fab
natanaelsc <natanael96@live.com>
2023-10-28 20:57:16 -0300

    chore: remove user.service

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,12 +3,11 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { CreateUserController } from './create-user/create-user.controller';
 import { CreateUserService } from './create-user/create-user.service';
 import { UserRepository } from './user.repository';
-import { UserService } from './user.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [CreateUserController],
-  providers: [UserService, CreateUserService, UserRepository],
-  exports: [UserService],
+  providers: [CreateUserService, UserRepository],
+  exports: [UserRepository],
 })
 export class UserModule {}
