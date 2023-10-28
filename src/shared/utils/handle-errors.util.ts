4931da1b6aa0b14c6b1f75172931000f62275fdd
natanaelsc <natanael96@live.com>
2023-10-28 20:52:58 -0300

    chore(exceptions): add error handler to prisma kown request errors

---
+++ src/shared/utils/handle-errors.util.ts
@@ -0,0 +1,24 @@
+import { Prisma } from '@prisma/client';
+import { P2002Exception } from '../exceptions/database.exceptions';
+import { UnknownErrorException } from '../exceptions/general.exceptions';
+import { isProduction } from './constants.util';
+
+export function prismaKnownRequestErrors(
+  error: Prisma.PrismaClientKnownRequestError,
+) {
+  const target = (error.meta?.target as Array<string>) || ['unknow_meta'];
+  if (error.code === 'P2002') throw new P2002Exception(target[0]);
+}
+
+export function unknownError(error: unknown) {
+  if (isProduction() !== true) console.error('unknownError', error);
+  throw new UnknownErrorException();
+}
+
+export function handleErrors(error: unknown) {
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    prismaKnownRequestErrors(error);
+  }
+
+  unknownError(error);
+}
