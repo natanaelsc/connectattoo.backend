1d04bf21897f5010bea09fc1fd26bcb408697369
natanaelsc <natanael96@live.com>
2023-10-28 17:07:31 -0300

    chore: create user class and dto

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,11 +3,13 @@ import { AppService } from 'src/app.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { CreateUserService } from './create-user/create-user.service';
+import { CreateUserController } from './create-user/create-user.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [UserController],
-  providers: [UserService, AppService],
+  controllers: [UserController, CreateUserController],
+  providers: [UserService, AppService, CreateUserService],
   exports: [UserService],
 })
 export class UserModule {}
