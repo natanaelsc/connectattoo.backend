2d8e1c839eb9975f9e3e201622d66ea20babd22b
natanaelsc <natanael96@live.com>
2023-10-28 20:45:28 -0300

    chore: remove app.service and app.controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 import { MailModule } from './modules/mail/mail.module';
@@ -10,9 +8,7 @@ import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule, UserModule, AuthModule, MailModule],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppService } from 'src/app.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CreateUserController } from './create-user/create-user.controller';
 import { CreateUserService } from './create-user/create-user.service';
+import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [CreateUserController],
-  providers: [UserService, AppService, CreateUserService],
+  providers: [UserService, CreateUserService, UserRepository],
   exports: [UserService],
 })
 export class UserModule {}
