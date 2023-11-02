7e8ac28453df6f450cd19c8420c70858ad4435ab
natanaelsc <natanael96@live.com>
2023-11-02 20:51:24 -0300

    chore: remove AuthRequest

--- src/modules/auth/models/AuthRequest.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from 'express';
-import { TattooClient } from 'src/modules/tattoo-client/tattoo-client';
-
-export interface AuthRequest extends Request {
-  user: TattooClient;
-}
