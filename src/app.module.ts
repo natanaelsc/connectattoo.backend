6d99010aa017f0160205e2bb8312d6bac051daa2
natanaelsc <natanael96@live.com>
2023-11-02 20:46:29 -0300

    chore: remove jwt guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './modules/auth/auth.module';
-import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 import { MailModule } from './modules/mail/mail.module';
 import { TattooClientModule } from './modules/tattoo-client/tattoo-client.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule, AuthModule, MailModule, TattooClientModule],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
-  ],
 })
 export class AppModule {}
