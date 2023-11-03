f6ddd1fd44c22162ed57c14986c16ca3b31b06fd
natanaelsc <natanael96@live.com>
2023-11-03 09:02:00 -0300

    chore: remove jwt strategy and passport

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,21 +1,19 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { TattooClientModule } from '../tattoo-client/tattoo-client.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
     TattooClientModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
     }),
   ],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
