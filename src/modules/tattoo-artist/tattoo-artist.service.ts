bc95c9bf6364db7919f319bf6388c35108cf6ba2
natanaelsc <natanael96@live.com>
2023-11-03 20:22:43 -0300

    chore: move duplicated code to UserService and remove unused methods

--- src/modules/tattoo-artist/tattoo-artist.service.ts
+++ src/modules/tattoo-artist/tattoo-artist.service.ts
@@ -1,7 +1,5 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
-import { CreateUser } from '../user/models/create-user';
-import { UserRepository } from '../user/user.repository';
+import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
 import { CreateUserArtistDto } from './dtos/create-user-artist.dto';
 import { TattooArtist } from './tattoo-artist';
 import { TattooArtistRepository } from './tattoo-artist.repository';
@@ -10,51 +8,15 @@ import { TattooArtistRepository } from './tattoo-artist.repository';
 export class TattooArtistService {
   constructor(
     private readonly tattooArtistRepository: TattooArtistRepository,
-    private readonly userRepository: UserRepository,
+    private readonly userService: UserService,
   ) {}
 
   async create(createUserDto: CreateUserArtistDto): Promise<TattooArtist> {
-    const email = createUserDto.email;
-    const emailAlreadyInUse = await this.userRepository.findByEmail(email);
-
-    if (emailAlreadyInUse !== null) {
-      throw new BadRequestException(`Usuário já cadastrado`);
-    }
-
-    const name = createUserDto.name.split(' ');
-    const firstName = name[0];
-    const lastName = name[1];
-
-    this.comparePasswords(
-      createUserDto.password,
-      createUserDto.passwordConfirmation,
-    );
-
-    const password = await bcrypt.hash(createUserDto.password, 10);
-
+    const createUser = await this.userService.create(createUserDto);
     delete createUserDto.password;
     delete createUserDto.passwordConfirmation;
-
-    const createUser = CreateUser.create(
-      firstName,
-      lastName,
-      email,
-      password,
-      createUserDto.birthDate,
-      createUserDto.address,
-    );
-
+    createUser.address = createUserDto.address;
     const user = await this.tattooArtistRepository.create(createUser);
     return user;
-  }
-
-  private comparePasswords(
-    password: string,
-    passwordConfirmation: string,
-  ): boolean {
-    if (password !== passwordConfirmation) {
-      throw new BadRequestException('Senhas não conferem');
-    }
-    return true;
   }
 }
--- src/modules/tattoo-client/tattoo-client.service.ts
+++ src/modules/tattoo-client/tattoo-client.service.ts
@@ -1,59 +1,21 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../user/dtos/create-user.dto';
-import { CreateUser } from '../user/models/create-user';
-import { UserRepository } from '../user/user.repository';
-import { TattooClient } from './tattoo-client';
+import { UserService } from '../user/user.service';
 import { TattooClientRepository } from './tattoo-client.repository';
+import { TattooClient } from './tattoo-client';
 
 @Injectable()
 export class TattooClientService {
   constructor(
     private readonly tattooClientRepository: TattooClientRepository,
-    private readonly userRepository: UserRepository,
+    private readonly userService: UserService,
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<TattooClient> {
-    const email = createUserDto.email;
-    const emailAlreadyInUse = await this.userRepository.findByEmail(email);
-
-    if (emailAlreadyInUse !== null) {
-      throw new BadRequestException(`Usuário já cadastrado`);
-    }
-
-    const name = createUserDto.name.split(' ');
-    const firstName = name[0];
-    const lastName = name[1];
-
-    this.comparePasswords(
-      createUserDto.password,
-      createUserDto.passwordConfirmation,
-    );
-
-    const password = await bcrypt.hash(createUserDto.password, 10);
-
+    const createUser = await this.userService.create(createUserDto);
     delete createUserDto.password;
     delete createUserDto.passwordConfirmation;
-
-    const createUser = CreateUser.create(
-      firstName,
-      lastName,
-      email,
-      password,
-      createUserDto.birthDate,
-    );
-
     const user = await this.tattooClientRepository.create(createUser);
     return user;
-  }
-
-  private comparePasswords(
-    password: string,
-    passwordConfirmation: string,
-  ): boolean {
-    if (password !== passwordConfirmation) {
-      throw new BadRequestException('Senhas não conferem');
-    }
-    return true;
   }
 }
