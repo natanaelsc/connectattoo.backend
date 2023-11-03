5fbd7bf92c68182fa0e261b8af6b99ef87daa999
natanaelsc <natanael96@live.com>
2023-11-03 19:52:05 -0300

    chore: remove unused method update from TattooClientRepository

--- src/modules/tattoo-client/tattoo-client.repository.ts
+++ src/modules/tattoo-client/tattoo-client.repository.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { handleErrors } from 'src/shared/utils/handle-errors.util';
 import { CreateUser } from '../user/models/create-user';
-import { UpdateUser } from '../user/models/update-user';
 import { TattooClient } from './tattoo-client';
 
 @Injectable()
@@ -12,17 +11,6 @@ export class TattooClientRepository {
   async create(createUser: CreateUser): Promise<TattooClient> {
     const tattooClient = await this.prisma.tattooClient
       .create({ data: createUser })
-      .catch((error) => handleErrors(error));
-    return tattooClient as TattooClient;
-  }
-
-  async update(updateUser: UpdateUser): Promise<TattooClient> {
-    const { id, email, isEmailConfirmed } = updateUser;
-    const tattooClient = await this.prisma.tattooClient
-      .update({
-        where: { id, email },
-        data: { isEmailConfirmed },
-      })
       .catch((error) => handleErrors(error));
     return tattooClient as TattooClient;
   }
