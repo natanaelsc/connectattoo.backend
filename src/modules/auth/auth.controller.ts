1f305662d9bab2d376eca7cc668914d6fe66cfb8
natanaelsc <natanael96@live.com>
2023-11-03 08:54:07 -0300

    feat: return http status code 200 in login route

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserLoginDto, UserToken } from './auth.dto';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { UserToken } from './auth.interface';
 import { AuthService } from './auth.service';
+import { UserLoginDto } from './dtos/user-login.dto';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
+  @HttpCode(HttpStatus.OK)
   async login(@Body() userLoginDto: UserLoginDto): Promise<UserToken> {
     const userToken = await this.authService.login(userLoginDto);
     return userToken;
