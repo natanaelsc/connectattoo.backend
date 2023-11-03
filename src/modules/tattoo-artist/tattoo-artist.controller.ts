a5d36d73f7a9b6324774bae800a3de56dd7978f0
natanaelsc <natanael96@live.com>
2023-11-03 19:34:33 -0300

    chore: add implementation to create method in TattooArtistController

--- src/modules/tattoo-artist/tattoo-artist.controller.ts
+++ src/modules/tattoo-artist/tattoo-artist.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AccountType } from '../auth/enums/account-type.enum';
+import { UserToken } from '../auth/interfaces/user-token.interface';
+import { JwtStrategy } from '../auth/strategies/jwt.strategy';
+import { MailService } from '../mail/mail.service';
+import { CreateUserArtistDto } from './dtos/create-user-artist.dto';
+import { TattooArtistService } from './tattoo-artist.service';
 
 @Controller('/users/artist')
-export class TattooArtistController {}
+export class TattooArtistController {
+  constructor(
+    private readonly tattooArtistService: TattooArtistService,
+    private readonly mailService: MailService,
+    private readonly jwtStrategy: JwtStrategy,
+  ) {}
+
+  @Post()
+  async create(
+    @Body() createUserArtistDto: CreateUserArtistDto,
+  ): Promise<UserToken> {
+    const user = await this.tattooArtistService.create(createUserArtistDto);
+    delete createUserArtistDto.password;
+    await this.mailService.sendVerificationLink(user.email);
+    const userToken = await this.jwtStrategy.getUserToken({
+      sub: user.id,
+      email: user.email,
+      type: AccountType.ARTIST,
+    });
+    return userToken;
+  }
+}
