5f59615007b3bdb7dcb948583d8bddc3bcac1e5c
natanaelsc <natanael96@live.com>
2023-11-03 19:29:31 -0300

    chore: import user module into auth module and inject user repository into auth service

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { TattooClientModule } from '../tattoo-client/tattoo-client.module';
+import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
-    TattooClientModule,
+    UserModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
