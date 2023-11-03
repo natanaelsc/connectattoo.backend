2e9403da2a5c291cebce10ac33b7d4163e4fb33a
natanaelsc <natanael96@live.com>
2023-11-03 09:02:47 -0300

    feat: return user token in user registration

--- src/modules/tattoo-client/tattoo-client.controller.ts
+++ src/modules/tattoo-client/tattoo-client.controller.ts
@@ -1,4 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { UserToken } from '../auth/auth.interface';
+import { AuthService } from '../auth/auth.service';
+import { UserType } from '../auth/enums/user-type.enum';
 import { MailService } from '../mail/mail.service';
 import { CreateUserDto } from './tattoo-client.dto';
 import { TattooClientService } from './tattoo-client.service';
@@ -8,11 +11,18 @@ export class TattooClientController {
   constructor(
     private readonly tattooClientService: TattooClientService,
     private readonly mailService: MailService,
+    private readonly authService: AuthService,
   ) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto): Promise<void> {
+  async create(@Body() createUserDto: CreateUserDto): Promise<UserToken> {
     const user = await this.tattooClientService.create(createUserDto);
     await this.mailService.sendVerificationLink(user.email);
+    const userToken = await this.authService.generateToken({
+      id: user.id,
+      email: user.email,
+      type: UserType.CLIENT,
+    });
+    return userToken;
   }
 }
--- src/modules/tattoo-client/tattoo-client.module.ts
+++ src/modules/tattoo-client/tattoo-client.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { TattooClientController } from './tattoo-client.controller';
 import { TattooClientRepository } from './tattoo-client.repository';
 import { TattooClientService } from './tattoo-client.service';
 
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   controllers: [TattooClientController],
   providers: [TattooClientRepository, TattooClientService],
   exports: [TattooClientRepository],
