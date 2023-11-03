168483f123ab1a9196285a71a1e5e7b744e36b20
natanaelsc <natanael96@live.com>
2023-11-03 20:21:50 -0300

    chore: export UserService from UserModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
-  providers: [UserRepository],
-  exports: [UserRepository],
+  providers: [UserRepository, UserService],
+  exports: [UserRepository, UserService],
 })
 export class UserModule {}
