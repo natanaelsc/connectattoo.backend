aff9431c2593db72e4e6d3176dcbcd5528e85763
natanaelsc <natanael96@live.com>
2023-11-03 19:35:11 -0300

    chore: import AuthModule and UserModule into TattooArtistModule

--- src/modules/tattoo-artist/tattoo-artist.module.ts
+++ src/modules/tattoo-artist/tattoo-artist.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
 import { TattooArtistController } from './tattoo-artist.controller';
 import { TattooArtistRepository } from './tattoo-artist.repository';
 import { TattooArtistService } from './tattoo-artist.service';
 
 @Module({
+  imports: [forwardRef(() => AuthModule), UserModule],
   providers: [TattooArtistService, TattooArtistRepository],
   controllers: [TattooArtistController],
 })
