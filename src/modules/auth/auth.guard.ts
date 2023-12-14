13a07484aa876bb62e1f72341a38ceb90b7af7bb
erickmarx <erickmarx5@gmail.com>
2023-12-13 22:51:49 -0300

    feat: guard block not confirmed email

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -8,6 +8,8 @@ import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 import { JwtStrategies } from '~/modules/auth/jwt.strategies';
 import { IS_PUBLIC_KEY } from '~/shared/constants/public.constant';
+import { JwtAuthPayload } from './interfaces/jwt-auth-payload.interface';
+import { AuthBusinessExceptions } from './exceptions/auth-business.exceptions';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -19,22 +21,34 @@ export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
 
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const isPublic = this.isPublic(context);
 
-    if (isPublic) {
-      return true;
-    }
+    if (!isPublic) return false;
 
     const token = this.extractTokenFromHeader(request);
 
     const payload = await this.jwtStrategies.auth.verify(token);
 
+    this.isEmailConfirmed(payload);
+
     request['user'] = payload;
 
     return true;
+  }
+
+  private isPublic(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    return isPublic;
+  }
+
+  private isEmailConfirmed(payload: JwtAuthPayload): void {
+    if (!payload.isEmailConfirmed) {
+      throw AuthBusinessExceptions.emailNotVerifiedException();
+    }
   }
 
   private extractTokenFromHeader(request: Request): string {
