88ddf021452c08a8256811b74ebfc102ad7ea105
lucas6g <lucas.trabalho1533@gmail.com>
2024-02-10 19:17:26 -0300

    feat(email): add email confirmation template from gitmodules

--- src/shared/adapters/mail/mail.service.ts
+++ src/shared/adapters/mail/mail.service.ts
@@ -1,21 +1,22 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
-import { render } from '@react-email/render';
-import Email from '../../../../emails/email-confirmation';
+import { EmailConfirmationTemplate } from '~/shared/adapters/mail/templates';
 
 @Injectable()
 export class MailService {
   constructor(private mailerService: MailerService) {}
   async sendConfirmationEmail(
     email: string,
-    firstName: string,
+    name: string,
     accessToken: string,
   ): Promise<void> {
-    const html = render(Email({url:`${process.env.EMAIL_CONFIRMATION_URL}?token=${accessToken}`,userName:firstName}));
     await this.mailerService.sendMail({
       to: email,
       subject: 'Confirmação de Cadastro',
-      html:html
+      html: await EmailConfirmationTemplate({
+        name,
+        confirmationLink: `${process.env.EMAIL_CONFIRMATION_URL}?token=${accessToken}`,
+      })
     });
   }
 }
