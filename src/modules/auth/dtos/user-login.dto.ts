4c23a55ad21fd707719e631d1223d0a9ff3ddfa9
erickmarx <erickmarx5@gmail.com>
2024-04-12 18:39:07 -0300

    Hotfix: Refactor code to use ISO8601 type for birthDate field

--- src/modules/auth/dtos/user-login.dto.ts
+++ src/modules/auth/dtos/user-login.dto.ts
@@ -1,9 +1,11 @@
-import { IsEmail, IsStrongPassword } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
 export class UserLoginDto {
+  @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @IsNotEmpty()
   @IsStrongPassword()
   password: string;
 }
--- src/modules/user/dtos/create-user.dto.ts
+++ src/modules/user/dtos/create-user.dto.ts
@@ -7,6 +7,7 @@ import {
   IsStrongPassword,
 } from 'class-validator';
 import { IRegisterUser } from 'src/modules/auth/interfaces/register-user.interface';
+import { ISO8601 } from 'src/shared/interface/ISO8601.type';
 
 export class RegisterUserDto implements IRegisterUser {
   @IsNotEmpty()
@@ -23,7 +24,7 @@ export class RegisterUserDto implements IRegisterUser {
 
   @IsNotEmpty()
   @IsDateString({ strict: true })
-  birthDate: string;
+  birthDate: ISO8601;
 
   @IsNotEmpty()
   @IsBoolean()
