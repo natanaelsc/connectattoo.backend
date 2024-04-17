92a50398c62d4dbfff313c9817dd5400ca5ea14e
erickmarx <erickmarx5@gmail.com>
2024-04-16 23:49:47 -0300

    refactor: general modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './modules/auth/auth.module';
-import { UserModule } from './modules/user/user.module';
 import { AdapterModule } from './shared/adapters/adapter.module';
 import { AppController } from './app.controller';
-import { PrismaModule } from './shared/adapters/prisma/prisma.module';
-import { ProfileModule } from './modules/profile/profile.module';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './shared/interceptor/exception-filter.interceptor';
-import { TagModule } from './modules/tag/tag.module';
-import { LocationModule } from './modules/location/location.module';
+import { ModulesModule } from './modules/modules.module';
 
 @Module({
-  imports: [
-    AdapterModule,
-    AuthModule,
-    UserModule,
-    PrismaModule,
-    ProfileModule,
-    TagModule,
-    LocationModule
-  ],
+  imports: [AdapterModule, ModulesModule],
   providers: [{ provide: APP_FILTER, useClass: HttpExceptionFilter }],
   controllers: [AppController],
 })
