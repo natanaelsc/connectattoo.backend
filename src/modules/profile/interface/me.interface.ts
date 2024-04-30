dfda6bf2099dd44e3c8f09cc0ccfdb639960c041
unknown <rafazel@outlook.com>
2024-04-29 22:28:37 -0300

    feat(profile): added email property to user profile interface object

--- src/modules/profile/interface/me.interface.ts
+++ src/modules/profile/interface/me.interface.ts
@@ -1,6 +1,7 @@
 export interface IMeProfile {
   displayName: string;
   username: string;
+  email: string;
   birthDate: string;
   imageProfile: any; // Será implementado posteriormente
   tags: { id: string; name: string }[];
