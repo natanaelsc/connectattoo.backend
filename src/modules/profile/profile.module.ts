79e16f4fbc278ec89a3de99da7259fb1f41cdc54
unknown <rafazel@outlook.com>
2024-04-29 22:30:36 -0300

    feat(profile): added user module to profile imports array

--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -5,9 +5,10 @@ import { ProfileRepository } from './profile.repository';
 import { PrismaModule } from '~/shared/adapters/prisma/prisma.module';
 import { TagModule } from '../tag/tag.module';
 import { StorageModule } from '../../shared/adapters/storage/storage.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [PrismaModule, TagModule, StorageModule],
+  imports: [PrismaModule, TagModule, StorageModule, UserModule],
   controllers: [ProfileController],
   providers: [ProfileService, ProfileRepository],
   exports: [ProfileService, ProfileRepository],
