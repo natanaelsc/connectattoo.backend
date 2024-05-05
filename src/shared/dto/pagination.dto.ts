a8d7d1bfd6ec26b5141931e5c62b930e9e8ac15c
erickmarx <erickmarx5@gmail.com>
2024-05-05 17:20:01 -0300

    fix: error of paginateDTO

--- src/shared/dto/pagination.dto.ts
+++ src/shared/dto/pagination.dto.ts
@@ -1,14 +1,20 @@
-import { IsOptional, IsPositive } from 'class-validator';
+import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
 import { IPagination } from '../interface/pagination.interface';
 import { Transform } from 'class-transformer';
 
 export class PaginationDTO implements IPagination {
+  @Transform(({ value }) => (value ? parseInt(value) : value), {
+    toClassOnly: true,
+  })
   @IsOptional()
   @IsPositive()
   limit: number;
 
+  @Transform(({ value }) => (value ? +value : 0), {
+    toClassOnly: true,
+  })
   @IsOptional()
-  @IsPositive()
-  @Transform(({ value }) => value || 0)
+  @Min(0)
+  @IsNumber()
   offset: number;
 }
