6dc864c3510096022edc8021e0706db8c63116b8
unknown <rafazel@outlook.com>
2024-06-17 19:25:17 -0300

    feat(profile): change UpdateImageDto to disable image upload limit size

--- src/modules/profile/dto/update-image.dto.ts
+++ src/modules/profile/dto/update-image.dto.ts
@@ -2,10 +2,12 @@ import { HttpStatus, ParseFilePipeBuilder } from '@nestjs/common';
 
 export const UpdateImageDTO = new ParseFilePipeBuilder()
   .addFileTypeValidator({ fileType: `^(image/jpeg|image/png)$` })
+  /* limite de upload de imagem desabilitado temporariamente
   .addMaxSizeValidator({
     maxSize: 1048576, // 1MB
     message: 'File size is too large',
   })
+  */
   .build({
     fileIsRequired: true,
     errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
